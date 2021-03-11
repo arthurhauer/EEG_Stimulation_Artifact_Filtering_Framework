2894a1a3605d53618fce1b62fff484f5d29c1e02
Arthur <46134064+arthurhauer@users.noreply.github.com>
2021-03-10 21:23:50 -0300

    Fix SNR estimation test

--- Matlab/SNR Estimation/snr_estimation.m
+++ Matlab/SNR Estimation/snr_estimation.m
@@ -4,13 +4,13 @@ function [snr,f] = snr_estimation(y,yl,Fs,windowValue)
     else if nargin==3
         window=kaiser(256,5);
         else
-            error("Número inválido de parâmetros");
+            error('Número inválido de parâmetros');
             return
         end
     end
     [csd,f1]=cpsd(y,yl,window);
     [psd,f2]=pwelch(y,window);
-    snr=(psd-abs(csd))/abs(csd);
+    snr=(abs(psd)-abs(csd))./abs(csd);
     f=f1;
 end
 
--- Matlab/SNR Estimation/test_arr.m
+++ Matlab/SNR Estimation/test_arr.m
@@ -1,15 +1,20 @@
 Fs=500;
 duration=10;
-frequency1=20;
-frequency2=25;
-amplitude=0.05;
+frequency=20;
+amplitude=1;
+stimulation_artifact_SNR=0.5;
 
-cleanSignal1=generate_sine_wave(frequency1,duration,amplitude,Fs);
-cleanSignal2=generate_sine_wave(frequency2,duration,amplitude,Fs);
-[estimatedCleanSNR,f]=snr_estimation(cleanSignal1,cleanSignal2,Fs);
-figure(1),subplot(2,1,1),plot(f,estimatedCleanSNR);
+basisSignal=generate_sine_wave(frequency,duration,amplitude,Fs); % Generate sinewave
 
-signal1=generate_repeatable_awgn(cleanSignal1,10);
-signal2=generate_repeatable_awgn(cleanSignal2,10);
-[estimatedSNR,f]=snr_estimation(signal1,signal2,Fs);
-figure(1),subplot(2,1,2),plot(f,estimatedSNR);
+cleanSignal1=awgn(basisSignal,10,'measured'); % Add white gaussian noise to act as commonly found noise in measurements
+cleanSignal2=awgn(basisSignal,10,'measured'); %
+
+[estimatedCleanSNR,f]=snr_estimation(cleanSignal1,cleanSignal2,Fs); % Estimate the 'clean signal' SNR
+figure(1),subplot(3,1,1),plot(f,estimatedCleanSNR);
+
+signal1=generate_repeatable_awgn(cleanSignal1,stimulation_artifact_SNR);% Add stimulation artifact noise
+signal2=generate_repeatable_awgn(cleanSignal2,stimulation_artifact_SNR);
+
+[estimatedSNR,f]=snr_estimation(signal1,signal2,Fs); % Estimate 'dirty' signal's SNR
+
+figure(1),subplot(3,1,2),plot(f,estimatedSNR);
